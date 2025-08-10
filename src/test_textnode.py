eb52308d8b52e51447e12de1b674d9110fd9504f
Heath Boutilier <heathboutilier@outlook.com>
2025-08-10 08:04:39 -0700

    C2L2

---
+++ src/test_textnode.py
@@ -0,0 +1,28 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(node, node2)
+    
+    def test_eq_2(self):
+        node = TextNode("This is a test", TextType.CODE)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_3(self):
+        node = TextNode("This is a test", TextType.CODE)
+        node2 = TextNode("This is a test", TextType.CODE, "url go heer")
+        self.assertNotEqual(node, node2)
+
+    def test_eq_4(self):
+        node = TextNode("This is a test", TextType.CODE, "url go heer")
+        node2 = TextNode("This is a test", TextType.CODE, "url go heer")
+        self.assertEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
