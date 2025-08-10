4a2113b8de20e77ea7877622b312f5aed1bfc6ad
Heath Boutilier <heathboutilier@outlook.com>
2025-08-10 07:52:54 -0700

    C2L1

---
+++ src/textnode.py
@@ -0,0 +1,20 @@
+from enum import Enum
+class TextType(Enum):
+    PLAIN_TEXT = 0
+    BOLD_TEXT = 1
+    ITALIC_TEXT = 2
+    CODE = 3
+    LINK = 4
+    IMAGE = 5
+
+class TextNode():
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eg__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
