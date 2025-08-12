d2022b311c7dbb77b028a83b0303f35acf19f3a9
Heath Boutilier <heathboutilier@outlook.com>
2025-08-12 08:26:55 -0700

    correction

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -26,7 +26,7 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
     def test_text(self):
-        node = TextNode("This is a text node", TextType.TEXT)
+        node = TextNode("This is a text node", TextType.PLAIN_TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
