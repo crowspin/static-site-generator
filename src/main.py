32a37db40495be634f04c97f16355fcaf8c0213c
Heath Boutilier <heathboutilier@outlook.com>
2025-08-12 08:26:08 -0700

    C2L6

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 from textnode import *
+from leafnode import LeafNode
 
 def main():
     tn = TextNode("This is some anchor text", TextType.LINK)
     print(tn)
+
+def text_node_to_html_node(text_node):
+    match (text_node.text_type):
+        case TextType.PLAIN_TEXT:
+            return LeafNode(value=text_node.text)
+        case TextType.BOLD_TEXT:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC_TEXT:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", props={"src":text_node.url, "alt":text_node.text})
+        case _:
+            raise Exception("Invalid TextType for conversion to HTML LeafNode.")
 
 main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from main import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +24,12 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a test", TextType.CODE, "url go heer")
         node2 = TextNode("This is a test", TextType.CODE, "url go heer")
         self.assertEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
